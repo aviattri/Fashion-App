8a14f14b322a16639e09361e43a014b40e3dd62a
aviattri <avirajattri8@gmail.com>
2023-10-11 15:48:31 +1100

    add trasitions

--- src/Home/FavouriteOutfits/FavouriteOutfits.tsx
+++ src/Home/FavouriteOutfits/FavouriteOutfits.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import theme, { Box } from "../../Components/Theme";
 import Header from "../../Components/Header";
 import { HomeNavigationProps } from "../../Components/Navigation";
+import {
+  Transitioning,
+  Transition,
+  TransitioningView,
+} from "react-native-reanimated";
 import Footer from "./Footer";
 import Outfit from "./Outfit";
 
@@ -25,8 +30,12 @@ const FavouriteOutfits = ({
   navigation,
 }: HomeNavigationProps<"FavouriteOutfits">) => {
   const width = (wWidth - theme.spacing.m * 3) / 2;
+  const left = React.useRef<Transitioning>(null);
+  const right = React.useRef<Transitioning>(null);
+
   const [FooterHeights, setFooterHeight] = React.useState(0);
   const [outfits, setOutfits] = React.useState(defaultOutfits);
+  const transition = <Transition.Change interpolation="easeInOut" />;
 
   return (
     <Box flex={1} backgroundColor="white">
@@ -44,18 +53,22 @@ const FavouriteOutfits = ({
       >
         <Box flexDirection="row">
           <Box marginRight="m">
-            {outfits
-              .filter((_, i) => i % 2 !== 0)
-              .map((outfit) => (
-                <Outfit key={outfit.id} outfit={outfit} width={width} />
-              ))}
+            <Transitioning.View ref={left} {...{ transition }}>
+              {outfits
+                .filter((_, i) => i % 2 !== 0)
+                .map((outfit) => (
+                  <Outfit key={outfit.id} outfit={outfit} width={width} />
+                ))}
+            </Transitioning.View>
           </Box>
           <Box>
-            {outfits
-              .filter((_, i) => i % 2 === 0)
-              .map((outfit) => (
-                <Outfit key={outfit.id} outfit={outfit} width={width} />
-              ))}
+            <Transitioning.View ref={right} {...{ transition }}>
+              {outfits
+                .filter((_, i) => i % 2 === 0)
+                .map((outfit) => (
+                  <Outfit key={outfit.id} outfit={outfit} width={width} />
+                ))}
+            </Transitioning.View>
           </Box>
         </Box>
       </ScrollView>
@@ -74,6 +87,9 @@ const FavouriteOutfits = ({
         <Footer
           label="Add to Favourites"
           onPress={() => {
+            //Filter out the selected outfits
+            left.current.animateNextTransition();
+            right.current.animateNextTransition();
             setOutfits(outfits.filter((outfit) => !outfit.selected));
           }}
         />
